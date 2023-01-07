9aedf1252b77e96a62231496af9db110881224b3
Howard Yen <hwrdpvt@gmail.com>
2023-01-07 12:47:12 -0500

    Created Weather Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import WeatherForm from './Components/WeatherForm/WeatherForm.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="Main__title">Weather Clothing Suggestion Generator</p>
+      <WeatherForm />
     </div>
   );
 }
---
+++ src/Components/WeatherForm/WeatherForm.js
@@ -0,0 +1,25 @@
+import './WeatherForm.scss';
+
+function WeatherForm() {
+    return(
+        <>
+            <form className="weather-form">
+                <label className="form__title">Date</label>
+                <input type="date" className="form__input"/>
+
+                <label className="form__title">Location</label>
+                <input type="text" className="form__input"/>
+
+                <label className="form__title">Gender</label>
+                <select id="gender_selectors" className="form__input">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+
+                <input type="submit"/>
+            </form>
+        </>
+    );
+}
+
+export default WeatherForm;
